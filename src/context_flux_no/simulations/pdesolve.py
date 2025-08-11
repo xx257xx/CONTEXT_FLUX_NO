3243104f19de15ff6327db4fd34c9246940c8237
Joon-Hyuk Ko <jhko725@kias.re.kr>
2025-08-11 15:33:50 +0900

    Wrote initial unified interface for the 1D Burgers equation as well as cubic conservation law

---
+++ src/context_flux_no/simulations/pdesolve.py
@@ -0,0 +1,53 @@
+from collections.abc import Callable
+from typing import Literal
+
+import numpy as np
+from clawpack import pyclaw
+from jaxtyping import Float
+
+from .pyclaw_utils import (
+    apply_initial_condition,
+    bc_from_string,
+    grid_centers_from_state,
+    make_controller,
+    make_domain,
+    solution_from_controller,
+)
+
+
+ClawSolver = pyclaw.ClawSolver1D | pyclaw.ClawSolver2D | pyclaw.ClawSolver3D
+
+
+def pdesolve_pyclaw(
+    solver: ClawSolver,
+    problem_data: dict[str, float],
+    ic_factory: Callable[[Float[np.ndarray, " Nx"]], Float[np.ndarray, " Nx"]],
+    x_span: tuple[float, float],
+    Nx: int,
+    t_span: tuple[float, float],
+    Nt: int,
+    bc: Literal["periodic"],
+) -> tuple[
+    Float[np.ndarray, "time dim x_grid"],
+    Float[np.ndarray, " time"],
+    Float[np.ndarray, " x_grid"],
+]:
+    # Need to change for >1D cases
+    solver.bc_lower[0] = solver.bc_upper[0] = bc_from_string(bc)
+    domain = make_domain(x_span, Nx)
+
+    state = pyclaw.State(domain, solver.num_eqn)
+    state.problem_data.update(problem_data)
+    apply_initial_condition(state, ic_factory)
+
+    controller = make_controller(state, domain, t_span, Nt)
+    controller.solver = solver
+
+    _ = controller.run()
+
+    u = solution_from_controller(controller)
+    t = np.linspace(
+        0, controller.tfinal, controller.num_output_times + 1, endpoint=True
+    )
+    (x_grid,) = grid_centers_from_state(state)
+    return u, t, x_grid
---
+++ src/context_flux_no/simulations/pyclaw_utils.py
@@ -0,0 +1,72 @@
+from collections.abc import Callable
+
+import numpy as np
+from clawpack import pyclaw
+from jaxtyping import Float
+
+
+def bc_from_string(bc_name: str) -> pyclaw.BC:
+    match bc_name:
+        case "periodic":
+            bc_pyclaw = pyclaw.BC.periodic
+        case _:
+            raise ValueError("Unrecognized boundary condition")
+    return bc_pyclaw
+
+
+def make_controller(
+    state: pyclaw.State, domain: pyclaw.Domain, t_span: tuple[float, float], Nt: int
+) -> pyclaw.Controller:
+    controller = pyclaw.Controller()
+    controller.solution = pyclaw.Solution(state, domain)
+    controller.tfinal = t_span[1] - t_span[0]
+    controller.num_output_times = Nt
+    # By default, keep the solution in memory and do not save
+    # See https://www.clawpack.org/pyclaw/output.html for details
+    controller.keep_copy = True
+    controller.output_format = None
+    return controller
+
+
+def solution_from_controller(
+    controller: pyclaw.Controller,
+) -> Float[np.ndarray, "time dim *grids"]:
+    return np.stack([f.state.q for f in controller.frames], axis=0)
+
+
+def grid_centers_from_state(
+    state: pyclaw.State,
+) -> list[Float[np.ndarray, " grid"]]:
+    grid = state.grid
+    grid_centers = [getattr(grid, dim_name).centers for dim_name in grid._dimensions]
+    return grid_centers
+
+
+def make_domain(x_span, Nx):
+    match x_span, Nx:
+        case tuple([float() | int(), float() | int()]), int():
+            dim = pyclaw.Dimension(*x_span, Nx, name="x")
+        case [tuple([float() | int(), float() | int()]), *_], int():
+            dim = [
+                pyclaw.Dimension(*x_span_i, Nx, name=f"x_{i}")
+                for i, x_span_i in enumerate(x_span)
+            ]
+        case [tuple([float() | int(), float() | int()]), *_], [int(), _]:
+            dim = [
+                pyclaw.Dimension(*x_span_i, Nx_i, name=f"x_{i}")
+                for i, (x_span_i, Nx_i) in enumerate(zip(x_span, Nx))
+            ]
+        case _:
+            raise ValueError("Unexpected combinattion of arguments")
+    return pyclaw.Domain(dim)
+
+
+def apply_initial_condition(
+    state: pyclaw.State,
+    ic_factory: Callable[
+        [Float[np.ndarray, " *x_grid"]], Float[np.ndarray, " *x_grid"]
+    ],
+) -> None:
+    grid_centers = grid_centers_from_state(state)
+    grid = np.meshgrid(*grid_centers)
+    state.q[0] = ic_factory(*grid)
